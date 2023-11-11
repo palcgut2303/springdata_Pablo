e0ac491cc39adfe8eae57500bdf152014d8ee131
palcgut2303 <palcgut2303@g.educaand.es>
2023-11-11 12:05:04 +0100

    Cambio Casa 12:04 11/11

--- src/main/java/com/example/springdata/entity/Empleado.java
+++ src/main/java/com/example/springdata/entity/Empleado.java
@@ -1,2 +1,30 @@
-package com.example.springdata.entity;public class Empleado {
+package com.example.springdata.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.boot.context.properties.bind.Name;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Empleado {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "birth_date",nullable = false)
+    private LocalDate birthdate;
+
+
+    @Column(name = "full_name",nullable = false)
+    private String FullName;
+
 }
