cb0a346b3092121f5dec5b5ab97b8cab4b168749
manana <palcgut2303@g.educaand.es>
2023-11-17 12:54:00 +0100

    Commit 12:53

--- src/main/java/com/example/springdata/entity/Empleado.java
+++ src/main/java/com/example/springdata/entity/Empleado.java
@@ -5,25 +5,24 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 import java.time.LocalDate;
 
 @Entity
+@Table(name = "empleados")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Empleado {
 
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "birth_date",nullable = false)
-    private LocalDate birthdate;
-
-
-    @Column(name = "full_name",nullable = false)
+    @Column(name = "full_name",unique = true)
     private String FullName;
 
+    @Column(name = "bith_date",nullable = false)
+    private LocalDate birthday;
+
+    private Boolean married;
 }
