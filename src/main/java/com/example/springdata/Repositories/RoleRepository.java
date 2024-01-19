7d5922e54574868396d854adf591da5a878a004b
manana <palcgut2303@g.educaand.es>
2024-01-19 13:54:04 +0100

    Commit 13:53 19/01/2024

--- src/main/java/com/example/springdata/Repositories/RoleRepository.java
+++ src/main/java/com/example/springdata/Repositories/RoleRepository.java
@@ -4,5 +4,8 @@ import com.example.springdata.entity.Role;
 import com.example.springdata.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RoleRepository extends JpaRepository<Role,Long> {
+    Optional<Role> findByName(String name);
 }
