e04df2d951551db2affe8736e3a4f5297d2d813c
manana <palcgut2303@g.educaand.es>
2024-01-31 14:22:21 +0100

    Commit 14:22 31/01/2024

--- src/main/java/com/example/springdata/security/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/example/springdata/security/filter/JwtAuthenticationFilter.java
@@ -2,17 +2,26 @@ package com.example.springdata.security.filter;
 
 import com.example.springdata.entity.Usuario;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.Jwts;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import javax.crypto.SecretKey;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter{
+
+    private static final SecretKey SECRET_KEY = Jwts.SIG.HS256.key().build();
 
     private AuthenticationManager authenticationManager;
 
@@ -39,6 +48,26 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 
         UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(username,password);
 
+
         return authenticationManager.authenticate(authenticationToken);
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+        User user = (User) authResult.getPrincipal();
+
+        String username = user.getUsername();
+        String token = Jwts.builder().subject(username).signWith(SECRET_KEY).compact();
+
+        response.addHeader("Authorization","Bearer "+token);
+
+        Map<String,String> body = new HashMap<>();
+        body.put("token",token);
+        body.put("username",username);
+        body.put("message","Las credenciales son correctas");
+
+        response.getWriter().write(new ObjectMapper().writeValueAsString(body));
+        response.setContentType("/application/json");
+        response.setStatus(200);
     }
 }
